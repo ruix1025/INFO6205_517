4b38e88d8a98c0e8846df400dbba1fc50d8f88dd
Rui Xia <xia.r@husky.neu.edu>
2019-04-14 16:26:04 -0400

    upload .ignore

---
+++ src/INFO6205_517/LearnAndDraw.java
@@ -0,0 +1,12 @@
+package INFO6205_517;
+
+import java.io.IOException;
+
+public class LearnAndDraw {
+
+	public static void main(String[] args) throws IOException {
+		GenerationTool gt = new GenerationTool();
+		System.out.println(gt.run());
+	}
+
+}
