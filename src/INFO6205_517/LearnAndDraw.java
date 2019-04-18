dffb1a15cef13fbf87221ba2f37f08bb3b5f23c9
Rui Xia <xia.r@husky.neu.edu>
2019-04-17 20:12:21 -0400

    多线程版本

--- src/INFO6205_517/LearnAndDraw.java
+++ src/INFO6205_517/LearnAndDraw.java
@@ -38,6 +38,7 @@ public class LearnAndDraw {
 		//System.out.println(gt.run());
 	}*/
 	
+	
 	public static void main(String[] args) throws Exception {
 		File pic = new File("test3.jpg");
 	    BufferedImage image = ImageIO.read(pic);
@@ -47,7 +48,7 @@ public class LearnAndDraw {
 		List<List<BI>> best_result = new ArrayList<List<BI>>();
 		
 		ExecutorService pool = Executors.newFixedThreadPool(7);
-		for(int i=0; i< HEIGHT; i++) {
+		for(int i=0; i< HEIGHT; i=i+4) {
 			//System.out.println("================");
 //			GenerationTool2 gt = new GenerationTool2(i, originalImage[i]);
 //			List<BI> tmp = gt.run();
@@ -57,18 +58,30 @@ public class LearnAndDraw {
 			//List<BI> tmp = gt3.run(originalImage[i]);
 			//best_result.add(tmp);
 			//System.out.println(tmp.size());
-			boolean[] originalImageLine = originalImage[i];
-			Callable<List<BI>> run = new Callable<List<BI>>(){
-				@Override
-				public List<BI> call() throws IOException, InterruptedException{
-					GenerationTool2 gt3 = new GenerationTool2();
-					List<BI> tmp = gt3.run(originalImageLine);
-					Thread.sleep(1000);
-					return tmp;
-				}
-			};
-			pool.submit(run); 	
-			best_result.add(run.call());
+			boolean[] originalImageLine_1 = originalImage[i];
+			InvolveLine il_1 = new InvolveLine();
+			il_1.setLine(originalImageLine_1);
+			pool.submit(il_1); 	
+			boolean[] originalImageLine_2 = originalImage[i+1];
+			InvolveLine il_2 = new InvolveLine();
+			il_2.setLine(originalImageLine_2);
+			pool.submit(il_2); 	
+			/*boolean[] originalImageLine_3 = originalImage[i+2];
+			InvolveLine il_3 = new InvolveLine();
+			il_3.setLine(originalImageLine_3);
+			pool.submit(il_3); 	
+			boolean[] originalImageLine_4 = originalImage[i+3];
+			InvolveLine il_4 = new InvolveLine();
+			il_4.setLine(originalImageLine_4);
+			pool.submit(il_4);*/
+			
+			int threadCount = ((ThreadPoolExecutor)pool).getActiveCount();
+			System.out.println("Running thread: " + threadCount);
+			
+			best_result.add(il_1.call());
+			best_result.add(il_2.call());
+			//best_result.add(il_3.call());
+			//best_result.add(il_4.call());
 		}
 		pool.shutdown();
 		
